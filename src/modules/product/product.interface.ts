f4ed4be8e3c4a458d668b321d1c2175433bc1f1b
smhasanjamil <smhasanjamil23@gmail.com>
2024-11-30 18:19:15 +0600

    Interface schema and model created for product

---
+++ src/modules/product/product.interface.ts
@@ -0,0 +1,11 @@
+export type TProduct = {
+  name: string;
+  brand: string;
+  price: number;
+  category:"Writing"|"Office Supplies"|"Art Supplies"|"Educational"|"Technology";
+  description: string;
+  quantity: number;
+  inStock: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
---
+++ src/modules/product/product.model.ts
@@ -0,0 +1,50 @@
+import { model, Schema } from "mongoose";
+import { TProduct } from "./product.interface";
+
+const productSchema = new Schema<TProduct>(
+  {
+    name: {
+      type: String,
+      required: [true, "Product name is required"],
+    },
+    brand: {
+      type: String,
+      required: [true, "Brand name is required"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: [0, "Price must be a valid price"],
+    },
+    category: {
+      type: String,
+      enum: {
+        values: [
+          "Writing",
+          "Office Supplies",
+          "Art Supplies",
+          "Educational",
+          "Technology",
+        ],
+        message: "{VALUE} is not a valid category",
+      },
+      required: [true, "Category is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Quantity is required"],
+      min: [0, "Enter a valid quantity"],
+    },
+    inStock: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const ProductModel = model<TProduct>("Product", productSchema);
